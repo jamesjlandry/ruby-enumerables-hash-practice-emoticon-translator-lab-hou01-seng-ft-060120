51991bb696197f2a90d1a29294183461b336db55
James Landry <james.j.landry@outlook.com>
2020-05-25 18:13:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,29 @@
 # require modules here
+require "yaml"
+require "pry"
 
-def load_library
+def load_library(yaml_file)
   # code goes here
+  emotis = YAML.load_file(yaml_file)
+  new_hash = {}
+  emotis.each do |name, symbols| 
+   new_hash[name] = {
+     english: symbols[0],
+     japanese: symbols[1]
+   }
+  end
+  new_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(yaml_file, eng_emoti)
   # code goes here
+  translation = ""
+  new_hash = load_library('./lib/emoticons.yml')
+  new_hash.each do |name, languages|
+      eng_emoti == languages[:english]
+      translation = languages[:japanese]
+      return translation
+    end
 end
 
 def get_english_meaning
